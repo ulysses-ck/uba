78630cecb4840f8702c75b8e7c0d7d9a6a920ee1
Ulises <ulisesapaza.280@gmail.com>
2025-10-16 04:12:14 -0300

    chore: ej2 s5

---
+++ fiuba/ing-informatica/cbc/pensamiento-computacional/semana5/ej-2.py
@@ -0,0 +1,11 @@
+# 2. Hacer una función que reciba un string y que lo invierta.
+
+def reverse_str(word: str):
+    return word[::-1]
+
+
+
+
+print(reverse_str("ana"))
+print(reverse_str("palastro"))
+print(reverse_str("asereje"))
